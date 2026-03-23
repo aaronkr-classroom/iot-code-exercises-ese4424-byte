478bd30fafae9f90ffaf2c4c3f8b87b882c6a4f6
ese4424-byte <ese4424@gmail.com>
2026-03-23 11:38:52 +0900

    Add files via upload

---
+++ code/3장/3_data_types.py
@@ -0,0 +1,14 @@
+# 3_data_types.py
+a = 10        # 정수형 (int)
+b = 3.14      # 상수형 (float)
+c = "hello"   # 문자열 (str)
+d = 'goodbye' # 문자열 (str)
+e = True      # 불리언(bool)
+
+print(f"{a}: {type(a)}") # f: fomat
+print(f"{b}: {type(b)}")
+print(f"{c}: {type(c)}")
+print(f"{d}: {type(d)}")
+print(f"{e}: {type(e)}")
+
+print(a, ":" , type(a))
