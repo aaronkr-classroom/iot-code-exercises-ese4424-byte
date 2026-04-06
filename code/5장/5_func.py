69564f7b4ff74ea5626174fac2de4cd8d91f2b11
2326055 이종민 <ese4424@gmail.com>
2026-04-06 11:23:17 +0900

    Add files via upload

---
+++ code/5장/5_func.py
@@ -0,0 +1,16 @@
+# 5_func.py
+
+def return_info(name, phone, address, email):
+    contact_info = f"연락처: \t{phone}\n이메일: \t{email}"
+    return f"이름: \t{name}\n{contact_info}\n주소: \t{address}"
+
+def print_info(name, phone, address, email=""):
+    contact_info = f"연락처: \t{phone}\n이메일: \t{email}"
+    print(f"이름: \t{name}\n{contact_info}\n주소: \t{address}")
+    
+print_info("Aaron", "010-5555-5555", "전주")
+person = return_info(
+    email ="hi@ut.ac.kr", phone = "010-5555-5555",
+    address = "한국교통대학교", name = "Aaron")
+print("")
+print(person)
